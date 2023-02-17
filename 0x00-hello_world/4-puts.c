268b914b8f44b94681e6cc0137cc8129cc7a3689
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-17 02:33:26 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-    /* Prints "Programming is like building a multilingual puzzle" */
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+/* Prints "Programming is like building a multilingual puzzle" */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
