30e516358b74c89d60f66c5dc9ad6816e4ece9e8
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-17 12:01:03 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{int d;
+for (d = 0; d <= 9; d++)
+{
+putchar(d + '0');
+putchar(',');
+putchar(' ');
+}
+
+
+putchar('\n');
+
+return (0);
+}
