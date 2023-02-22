63dab2bdad91aa64f8bddafa38abaf1926858f6a
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-22 12:44:48 -0800

    3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,6 +10,7 @@ int _putchar(char c);
 
 /* Function prototypes */
 int function_name(int arg1, char arg2);
+int _islower(int c);
 void another_function(char arg3, int arg4);
 void print_alphabet_x10(void);
 void print_alphabet(void);
