d28fb5421f4d1466ad0a2688d98c9dac4c5be1fb
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-22 12:13:20 -0800

    0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+/* putchar prototype */
+int _putchar(char c);
+
+/* Function prototypes */
+int function_name(int arg1, char arg2);
+void another_function(char arg3, int arg4);
+
+#endif /* MAIN_H */
