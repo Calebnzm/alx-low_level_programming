34b3352c7e1c25b4ca659f5e58dfee6a1fdf5583
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-23 10:17:33 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9') {
+        return 1;
+    }
+    return 0;
+}
+
