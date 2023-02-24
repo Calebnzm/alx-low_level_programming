e7c0f5cc0eda2d75009db112acde81a9330665e9
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-24 01:43:25 -0800

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of size n using the # character.
+ *
+ * @size: The size of the triangle to print.
+ */
+void print_triangle(int size)
+{
+int row, column;
+
+if (size <= 0) {
+_putchar('\n');
+return;
+}
+
+for (row = 1; row <= size; row++) {
+for (column = 1; column <= size - row; column++) {
+_putchar(' ');
+}
+for (column = 1; column <= row; column++) {
+_putchar('#');
+}
+_putchar('\n');
+}
+}
