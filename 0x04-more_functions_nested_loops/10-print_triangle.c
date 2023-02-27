1d33344981f388228556cf74b7c2cc9a410784a0
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-02-27 03:54:23 -0800

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,16 +9,20 @@ void print_triangle(int size)
 {
 int row, column;
 
-if (size <= 0) {
+if (size <= 0)
+{
 _putchar('\n');
 return;
 }
 
-for (row = 1; row <= size; row++) {
-for (column = 1; column <= size - row; column++) {
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size - row; column++)
+{
 _putchar(' ');
 }
-for (column = 1; column <= row; column++) {
+for (column = 1; column <= row; column++)
+{
 _putchar('#');
 }
 _putchar('\n');
