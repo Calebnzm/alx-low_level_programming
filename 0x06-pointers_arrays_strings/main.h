c8e156c8689e066f92b6273cb08e1d431fb7fa08
Caleb Nzioka Muthama <calebnzioka2027@gmail.com>
2023-03-02 10:09:12 -0800

    4

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
