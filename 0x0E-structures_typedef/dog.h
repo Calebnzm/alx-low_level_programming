9aa57a18bf41c05a3e55a17fb502f6ec3f6eb2b6
root <calebnzioka2027@gmail.com>
2023-06-20 08:23:16 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+* struct dog - to store the details of a dog
+* @name: type = char *
+* @age: type = float
+* @owner: type = char *
+*
+* Desription: to store the details of a dog with the above elements
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
