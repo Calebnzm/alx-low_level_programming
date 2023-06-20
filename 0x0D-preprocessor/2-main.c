27e5c4d88e88ec10c159931e423ab7d786d41e87
root <calebnzioka2027@gmail.com>
2023-06-20 05:30:11 -0700

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,27 +1,5 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
-* filename - gives the name of the file
-*
-* Return: the name of the file
-*/
-char *filename(void)
-{
-return (__FILE__);
-}
+#include <stdio.h>
+#define FILENAME __FILE__
 
 /**
 * main - prints the name of the file
@@ -30,15 +8,6 @@ return (__FILE__);
 */
 int main(void)
 {
-int j;
-char *a;
-j = 0;
-a = filename();
-while (a[j] != '\0')
-{
-_putchar(a[j]);
-j++;
-}
-_putchar('\n');
+printf("%s\n", FILENAME);
 return (0);
 }
