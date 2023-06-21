1b6254969b67b297332f6fdd9a21e4d20c85e196
root <calebnzioka2027@gmail.com>
2023-06-21 04:14:14 -0700

    0

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-if (array != NULL && action != NULL && size >= 0)
+if (array != NULL && action != NULL && size != 0)
 {
 long unsigned int i;
 for (i = 0; i < size; i++)
