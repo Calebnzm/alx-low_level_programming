e96df8fb0d52b66f65c5be86c11463e3eb6751b0
root <calebnzioka2027@gmail.com>
2023-09-08 08:47:12 -0700

    Task 4 correction

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -21,7 +21,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			return strdup((current->value));
+			return (current->value);
 		}
 		current = current->next;
 	}
