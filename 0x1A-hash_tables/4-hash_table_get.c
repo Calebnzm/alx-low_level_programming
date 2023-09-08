365345e77367a2b8d6d634bf96a91369b3f316af
root <calebnzioka2027@gmail.com>
2023-09-08 08:44:46 -0700

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - gets value at certain index in ahash table
+* @ht: hash table to retreive from
+* @key: the key to get its value
+*
+* Return: value of key(success) null(failure)
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return strdup((current->value));
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
